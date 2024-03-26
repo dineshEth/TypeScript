aa33789cda2f3655c16cff504faf9356b525da31
Dinesh <Dineshchauhan416pd@gmail.com>
2024-03-26 20:29:11 +0530

    variable and datatype

---
+++ 01Basics/02intro.js
@@ -0,0 +1,23 @@
+"use strict";
+/***TYPES  */
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * Number , String, Boolean, Null, Undefined,
+ */
+var schoolName = "VishwaGurukul English";
+var CGPA = 9.56;
+var color = '#2325ea';
+// number
+var userId = 32458; // integer
+var piValue = 3.14; // float
+var value = 325; // type can be ignored here
+// boolean
+var isRegistered = true;
+var isLoggedIn = false;
+/**
+ * SYNTAX
+ * let variableName : type = value;
+ * const variableName : type = value;
+ */
+var fullName = "Dinesh";
+var age = 12;
---
+++ 01Basics/02intro.ts
@@ -0,0 +1,33 @@
+/***TYPES  */
+
+/**
+ * Number , String, Boolean, Null, Undefined,
+ */
+let schoolName : string = "VishwaGurukul English"
+let CGPA : number = 9.56;
+let color : any = '#2325ea';
+let superHeroes : string[] = ["Spiderman","AntMan"]   // string type arary
+let superNumber : Array<number> = [12,13]
+
+// number
+let userId : number = 32458;   // integer
+let piValue : number = 3.14;   // float
+let value = 325;    // type can be ignored here
+
+// boolean
+let isRegistered : boolean  = true;
+let isLoggedIn = false;
+
+
+
+/**
+ * SYNTAX 
+ * let variableName : type = value;
+ * const variableName : type = value;
+ */
+const fullName : string = "Dinesh"
+let age : number = 12; 
+
+// fullName = 122   : throws an error,
+
+export {}
